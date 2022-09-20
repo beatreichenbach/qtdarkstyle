9121a7323df357ad24417c3b53fbc4cc9ef067a3
Beat Reichenbach <info@artofbeat.com>
2022-09-19 20:43:46 -0700

    Add package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='qtdarkstyle',
+    version='0.1',
+    description='A dark style for Qt',
+    author='Beat Reichenbach',
+    packages=[
+        'qtdarkstyle'
+        ],
+    install_requires=[
+        'PySide2',
+        ]
+)
